f7c6159843b1342e7c7411f1d05b80bd737d7c9e
wojnareric <62747840+wojnareric@users.noreply.github.com>
2020-03-27 06:56:45 -0400

    Initial Plugs

---
+++ Required Comments/ui/js/requireComments.js
@@ -0,0 +1,137 @@
+/*
+ * arrive.js
+ * v2.4.1
+ * https://github.com/uzairfarooq/arrive
+ * MIT licensed
+ *
+ * Copyright (c) 2014-2017 Uzair Farooq
+ */
+
+//Load the Arrive library for later user
+
+/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+
+    Usage example:
+
+        waitForKeyElements (
+            "div.comments"
+            , commentCallbackFunction
+        );
+
+        //--- Page-specific function to do what we want when the node is found.
+        function commentCallbackFunction (jNode) {
+            jNode.text ("This comment changed by waitForKeyElements().");
+        }
+
+    IMPORTANT: This function requires your script to have loaded jQuery.
+*/
+function waitForKeyElements (
+    selectorTxt,    /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+    actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == "undefined")
+        targetNodes     = $(selectorTxt);
+    else
+        targetNodes     = $(iframeSelector).contents ()
+                                           .find (selectorTxt);
+
+    if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else {
+        btargetsFound   = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                    waitForKeyElements (    selectorTxt,
+                                            actionFunction,
+                                            bWaitOnce,
+                                            iframeSelector
+                                        );
+                },
+                300
+            );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
+
+
+
+var manipulate = function(){
+		jQuery(jQuery("h4").get(2)).after("<b><font color=\"red\">Note: You Must Add Comments Justifying Each Request Before You Will Be Allowed to Submit</font></b>");
+		var dom = jQuery("#submitBtn");
+		angular.element(".sp-ui-app").injector().invoke(['$compile', function($compile) {
+		angular.element(dom).attr("ng-click", "modifiedSubmit()");
+		//var workitemscope = angular.element("sp-work-item-form").scope();
+		var scope = angular.element(dom).scope(); 
+		scope.$apply(function () {
+			  scope.modifiedSubmit = function(button) {
+			  missingComment = false;
+			  reqs = scope.reviewCtrl.getRequestedItems()
+			  reqs.forEach(
+				function(item, index, array) { 
+					if (!item.comment) missingComment = true;
+			  });
+			  if (missingComment) {
+				alert("You have not supplied comments for each requested item. Please review and add any missing comments");
+			  }
+			  else {
+				scope.reviewCtrl.submit();
+			  }
+		}	
+		$compile(dom)(scope);
+
+		});
+		} 
+	]
+	);
+}
+
+waitForKeyElements("#submitBtn", manipulate, true);
---
+++ WorkItemInject/ui/js/workItemInject.js
@@ -0,0 +1,123 @@
+/*
+ * arrive.js
+ * v2.4.1
+ * https://github.com/uzairfarooq/arrive
+ * MIT licensed
+ *
+ * Copyright (c) 2014-2017 Uzair Farooq
+ */
+
+//Load the Arrive library for later user
+
+/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
+    that detects and handles AJAXed content.
+
+    Usage example:
+
+        waitForKeyElements (
+            "div.comments"
+            , commentCallbackFunction
+        );
+
+        //--- Page-specific function to do what we want when the node is found.
+        function commentCallbackFunction (jNode) {
+            jNode.text ("This comment changed by waitForKeyElements().");
+        }
+
+    IMPORTANT: This function requires your script to have loaded jQuery.
+*/
+
+function waitForKeyElements (
+    selectorTxt,    /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+    actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
+
+    if (typeof iframeSelector == "undefined")
+        targetNodes     = $(selectorTxt);
+    else
+        targetNodes     = $(iframeSelector).contents ()
+                                           .find (selectorTxt);
+
+    if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else {
+        btargetsFound   = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                    waitForKeyElements (    selectorTxt,
+                                            actionFunction,
+                                            bWaitOnce,
+                                            iframeSelector
+                                        );
+                },
+                300
+            );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
+
+
+
+var manipulate = function(){
+		//jQuery(jQuery("h4").get(2)).before("<b><font color=\"red\">Note: You Must Add Comments Justifying Each Request Before You Will Be Allowed to Submit</font></b>");
+		//$("#btnDetailsApproval0").before("<b><font color=\"red\">Note: Please read all comments </font></b>");
+		//jQuery(jQuery("btnDetailsApproval0").get(1)).before("<b><font color=\"red\">Note: Please read all comments </font></b>");
+		//jQuery(jQuery("btnDetailsApproval0")).before("<b><font color=\"red\">Note: Please read all comments </font></b>");
+		jQuery("#btnDetailsApproval0").before("<b><font color=\"red\">Note: Please read all comments prior to making a decision </font></b>");
+				//Query(jQuery("btnDetailsApproval0").before("<b><font color=\"red\">Note: Please read all comments </font></b>");
+		//$("#btnDetailsApproval0").before("<b><font color=\"red\">Note: Please read all comments </font></b>");
+		//document.getElementById('btnDetailsApproval0').before("<b><font color=\"red\">Note: Please read all comments </font></b>");
+}
+/*
+$(window).bind("load", function() {
+   manipulate();
+});
+*/
+waitForKeyElements("#btnDetailsApproval0", manipulate, true);
